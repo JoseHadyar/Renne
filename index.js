18e9283b84cc7bfc6d34668ad2b200d4bf561529
Jose Hadyar <j.hadyar@gmail.com>
2022-07-13 23:10:44 -0300

    Actualizacion 2.0 Pt1 PF

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// function convertir(){
-//     let valore = parseInt(document.getElementById("valor").value)
-//     let resultado = 0
-//     let dolar = 223
-//     let euro = 243
-
-//     if (document.getElementById("uno").checked){
-//         resultado = valore / dolar
-//         alert("El cambio de pesos(AR) a Dolares es: $" + resultado.toFixed(2) )
-//     }
-//     else if (document.getElementById("dos").checked){
-//         resultado = valore / euro
-//         alert("El cambio de pesos(AR) a Euro es: €" + resultado.toFixed(2) )
-//     }
-//     else{
-//         alert("¡Complete el formulario!")
-//     }
-        
-// }
 
 function capturar(){
     // console.log("capturado")
